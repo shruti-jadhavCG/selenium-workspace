b422db004673ceab6fb3c2df261e7c9d38b52761
shruti <shrutijadhav979@gmail.com>
2024-06-16 14:26:27 +0530

    pushing new assignment

---
+++ MavenProject/src/main/java/basic/MavenProject/class1.java
@@ -0,0 +1,17 @@
+package basic.MavenProject;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=2;
+		System.out.println(a+b);
+		
+		ChromeDriver driver = new ChromeDriver();
+
+	}
+
+}
---
+++ Selenium_Java/src/test/testcase1.java
@@ -0,0 +1,25 @@
+package test;
+import java.io.FileInputStream;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class testcase1 
+{
+
+	@Test
+	public static void login()
+	{
+		EdgeDriver driver =new EdgeDriver();
+		driver.get("https://www.amazon.in");
+		driver.manage().window().maximize();
+		WebElement hoverver = driver.findElement(By.xpath("//span[@class='nav-line-2 ']"));
+		WebElement signin = driver.findElement(By.xpath("//span[@class='nav-action-inner']"));
+		Actions a = new Actions(driver);
+		a.moveToElement(hoverver);
+		signin.click();
+	}
+}
